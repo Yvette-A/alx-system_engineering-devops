e69b6140513bade1fa623b75a9650c1b48d92171
Yvette Achieng <achiengyvette@gmail.com>
2023-10-23 05:51:56 -0700

    adding files

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Exports data to JSON format."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    tasks = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": i.get("title"),
+                "completed": i.get("completed"),
+                "username": username
+            } for i in tasks]}, jsonfile)
